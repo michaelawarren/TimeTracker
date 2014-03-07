31f20d661db7badcc1ab03b20109b66ac02fac88
MichaelAWarren <MichaelAWarren@outlook.com>
2014-03-07 08:17:17 -0700

    Adding Basic Commands

--- TimeTracker/src/main/java/com/personal/timetracker/App.java
+++ TimeTracker/src/main/java/com/personal/timetracker/App.java
@@ -1,13 +1,117 @@
 package com.personal.timetracker;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to Time tracker!" );
-    }
+
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Time tracker!");
+		if (args.length == 1)
+		{
+			Command command = null;
+			System.out.println(args[0]);
+			try
+			{
+				command = Command.valueOf(args[0]);
+			}
+			catch (Exception ex)
+			{
+				System.out.println("Invalid Command");
+				return;
+			}
+
+			switch (command)
+			{
+				case START:
+				case END:
+					write(command, Calendar.getInstance().getTime());
+					break;
+				case LOG_TIME:
+				case BUILD_REPORT:
+				case VIEW_LOG:
+				default:
+					throw new UnsupportedOperationException();
+			}
+		}
+	}
+
+	public static void readFile()
+	{
+		BufferedReader br = null;
+
+		try
+		{
+			String sCurrentLine;
+
+			br = new BufferedReader(new FileReader("testing.txt"));
+
+			while ((sCurrentLine = br.readLine()) != null)
+			{
+				System.out.println(sCurrentLine);
+			}
+
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				if (br != null)
+				{
+					br.close();
+				}
+			}
+			catch (IOException ex)
+			{
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	private static void write(Command command, Date time)
+	{
+		try
+		{
+			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			String content = command +", " + dateFormat.format(time) + "\n";
+
+			File file = new File("C:/cygwin64/home/Michael/dev/log.csv");
+
+			// if file doesnt exists, then create it
+			if (!file.exists())
+			{
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+
+			System.out.println("Done");
+
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
---
+++ TimeTracker/src/main/java/com/personal/timetracker/Command.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.personal.timetracker;
+
+/**
+ *
+ * @author Michael
+ */
+public enum Command {
+    START,
+    END,
+	LOG_TIME,
+    BUILD_REPORT,
+    VIEW_LOG;
+}
