627a4be53cdc01399d7e7f5d6a9b5ccbab025d4f
MichaelAWarren <MichaelAWarren@outlook.com>
2014-03-07 08:19:49 -0700

    adding html doc support

---
+++ TimeTracker/src/main/java/com/personal/timetracker/html/HtmlTag.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.personal.timetracker.html;
+
+/**
+ *
+ * @author Michael
+ */
+public enum HtmlTag
+{
+	HTML("html"),
+	HEAD("head"),
+	BODY("body"),
+	DIV("div"),
+	TABLE("table"),
+	ROW("tr"),
+	COL("td");
+	
+}
